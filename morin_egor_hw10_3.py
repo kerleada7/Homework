8029ebced3f39841dd78408cb25240073f947ea9
kerleada7 <kerleada7@mail.ru>
2021-04-07 10:40:53 +0300

    Lesson 10

--- morin_egor_hw10_3.py
+++ morin_egor_hw10_3.py
@@ -27,4 +27,70 @@
     (https://pythonworld.ru/osnovy/peregruzka-operatorov.html).
 '''
 
-"НЕ УСПЕЛ!)"
+
+class Cell:
+    def __init__(self, cell_count):
+        try:
+            if cell_count <= 0:
+                raise ValueError('Количество клеток должно быть больше 0')
+
+            self.cell_count = int(cell_count)
+        except:
+            raise ValueError('Количество клеток должно быть задано числом')
+
+    def __str__(self):
+        return str(f' Количество клеток: {self.cell_count}')
+
+    def __add__(self, other):
+        if type(other) != Cell:
+            ValueError('Сложение выполняется только клетка с клеткой')
+        return Cell(self.cell_count + other.cell_count)
+
+    def __sub__(self, other):
+        if type(other) != Cell:
+            ValueError('Вычитание выполняется только клетка с клеткой')
+
+        if self.cell_count < other.cell_count:
+            ValueError('Клетка не может быть отрицательной, но может быть мертвой = 0')
+
+        return Cell(self.cell_count - other.cell_count)
+
+    def __mul__(self, other):
+        if type(other) != Cell:
+            ValueError('Умножение выполняется только клетка с клеткой')
+
+        return Cell(self.cell_count * other.cell_count)
+
+    def __floordiv__(self, other):
+        if type(other) != Cell:
+            ValueError('Деление выполняется только клетка с клеткой')
+
+        if other.cell_count == 0:
+            ValueError('Делить на мертвую клетку нельзя')
+
+        return Cell(self.cell_count // other.cell_count)
+
+    def __truediv__(self, other):
+        if type(other) != Cell:
+            ValueError('Деление выполняется только клетка с клеткой')
+
+        if other.cell_count == 0:
+            ValueError('Делить на мертвую клетку нельзя')
+
+        return Cell(self.cell_count % other.cell_count)
+
+    @staticmethod
+    def make_order(cell, row_count):
+        print(f'Количество клеток: {cell.cell_count}; Количество клеток в ряду: {row_count}')
+        return '\n'.join('*' * row_count for i in range(cell.cell_count // row_count)) + '\n' + '*' * (cell.cell_count % row_count)
+
+
+cell_mobius = Cell(22)
+print(cell_mobius)
+
+cell_sirius = Cell(11)
+print(cell_sirius)
+
+print(cell_mobius // cell_sirius)
+
+print(Cell.make_order(cell_mobius, 5))
